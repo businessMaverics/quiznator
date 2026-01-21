3057fdd25e46429f24d4547f989f07554ae3639d
BusinessMaverics <business@maverics.com>
2026-01-21 12:49:51 +0100

    Install Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Cinzel, Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const cinzel = Cinzel({
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${cinzel.variable} ${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
